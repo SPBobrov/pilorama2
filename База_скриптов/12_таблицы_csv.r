56408e4bfc83d129a4c112f152936ddcc1d766dd
SPBobrov <spb.74@mail.ru>
2021-12-14 18:44:37 +0300

    скрипт закончил, написал Боре

--- База_скриптов/12_таблицы_csv.r
+++ База_скриптов/12_таблицы_csv.r
@@ -1,3 +1,3 @@
 library(tidyverse)
-tabl2 <- read.csv("База_данных/tabl2.csv")
-tabl2
+tabl2 <- read.csv("База_данных/tabl2.csv") #чтение файла csv
+write.csv(tabl1, "База_данных/tabl1.csv") #запись файла csv
--- Рабочие_скрипты/исходные данные.r
+++ Рабочие_скрипты/исходные данные.r
@@ -5,6 +5,7 @@ gs4_auth(email = "stollstoy@gmail.com") #авторизироваться
 # скачать таблицу склада в переменную
 ТАБЛИЦА_СКЛАДА=read_sheet(ss = "https://docs.google.com/spreadsheets/d/1Whng31XfivMScu67JFtzteEoeju8zhVtWNk9Y6Bt2Fk/edit#gid=1748636237", sheet="Новый (2019)", range="A:AN")
 
+# более быстрый способ, применяемый в RFlow
 ТАБЛИЦА_СКЛАДА1 <- range_speedread(ss="https://docs.google.com/spreadsheets/d/1Whng31XfivMScu67JFtzteEoeju8zhVtWNk9Y6Bt2Fk/edit?ts=5bd41fba#gid=1748636237", range="a:ap", col_types = "ccccccccnnnnnnnnnnncccccccccccccncccccccn?")
 
 # создать R объект
